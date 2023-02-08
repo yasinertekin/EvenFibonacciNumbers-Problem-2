e8974f2a75a098d05cfccafabcf0340144046199
Yasin Ertekin <111593895+yasinertekin@users.noreply.github.com>
2023-02-08 12:21:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Xml.Linq;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList ÇiftSayilar = new ArrayList();
+
+            int BirinciSayi = 1;
+            int İkinciSayi = 1;
+            int ÜçüncüSayi = 0;
+            int toplam = 0;
+
+            while (ÜçüncüSayi < 4000000)
+            {
+
+                if (ÜçüncüSayi % 2 == 0)
+                {
+                    toplam = toplam + ÜçüncüSayi;
+                }
+                ÜçüncüSayi = BirinciSayi + İkinciSayi;
+                BirinciSayi = İkinciSayi;
+                İkinciSayi = ÜçüncüSayi;
+                
+
+            }
+            Console.WriteLine(toplam);
+            
+
+
+        }
+    }
+}
